370aecff12fb09614d6ddacd498e8c461f6616c5
tian <t18215595124@gmail.com>
2019-05-24 12:56:36 +0800

    Update trainer.py

--- fairseq/trainer.py
+++ fairseq/trainer.py
@@ -259,8 +259,6 @@ class Trainer(object):
                     sample, self.model, self.criterion, self.optimizer,
                     ignore_grad
                 )
-                print('--------in train_step,logoutput---')
-                print('log.acc {}'.format(logging_output['acc'] ))
                 if not ignore_grad:
                     logging_outputs.append(logging_output)
                     sample_sizes.append(sample_size)
@@ -352,11 +350,9 @@ class Trainer(object):
             )
             self.meters['train_loss'].update(logging_output.get('loss', 0), sample_size)
             if 'train_acc' in self.meters:
-                print('-----trainer get train_acc meter---')
+          
                 self.meters['train_acc'].update(
                     logging_output.get('acc', 0), sample_size)
-                print('in logging_output acc is {}'.format(logging_output.get('acc',0)))
-                print('in meters is {}'.format(self.meters['train_acc'].val))
 
             if 'nll_loss' in logging_output:
                 self.meters['train_nll_loss'].update(logging_output.get('nll_loss', 0), ntokens)

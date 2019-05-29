648fa33ce661ff92cd1eeaf0d06939fe66b88505
tian <t18215595124@gmail.com>
2019-05-29 14:44:08 +0800

    Update group_transformer_train_trans.py

--- fairseq/models/group_transformer_train_trans.py
+++ fairseq/models/group_transformer_train_trans.py
@@ -14,7 +14,7 @@ import torch.nn.functional as F
 from fairseq import options, utils
 from fairseq.models import (
     FairseqEncoder,
-    FairseqIncrementalDecoder,
+    GroupIncrementalDecoder,
     FairseqEncoderDecoderModel,
     register_model,
     register_model_architecture,

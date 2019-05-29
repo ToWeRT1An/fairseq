8cf21e6dc5b59b6bb2ec17f2f829ac7eff54c0ab
tian <t18215595124@gmail.com>
2019-05-29 19:53:44 +0800

    Update group_transformer_entropy.py

--- fairseq/criterions/group_transformer_entropy.py
+++ fairseq/criterions/group_transformer_entropy.py
@@ -61,7 +61,7 @@ class GroupTransformerEntropy(FairseqCriterion):
         print('----null-loss1')
         print(nll_loss1)
         print('topk of nll_loss1:',torch.topk(nll_loss1,10)[0])
-        print('-lprobs1.sum(dim=-1, keepdim=True)',-lprobs1.sum(dim=-1, keepdim=True))
+        print('-lprobs1.sum(dim=-1, keepdim=True)',-lprobs1.sum(dim=-1, keepdim=True)[non_pad_mask])
         smooth_loss1 = -lprobs1.sum(dim=-1, keepdim=True)[non_pad_mask]
         nll_loss2 = -lprobs2.gather(dim=-1, index=target2)
         smooth_loss2 = -lprobs2.sum(dim=-1,keepdim=True)

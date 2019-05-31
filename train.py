238421156034723f1e393b8cec48dacba5cafde0
tian <t18215595124@gmail.com>
2019-05-31 19:49:37 +0800

    Update train.py

--- train.py
+++ train.py
@@ -147,7 +147,7 @@ def train(args, trainer, task, epoch_itr):
                 extra_meters[k].update(v)
             stats[k] = extra_meters[k].avg
         print('-----stats is ')
-        print(stats)
+        print(stats['train_acc'].val)
         progress.log(stats, tag='train', step=stats['num_updates'])
 
         # ignore the first mini-batch in words-per-second calculation

688642bfbf3a152f04af5dd360b87432d1e133e1
tian <t18215595124@gmail.com>
2019-06-02 18:35:19 +0800

    add eos loss to trainsformer

--- fairseq/criterions/group_transformer_entropy.py
+++ fairseq/criterions/group_transformer_entropy.py
@@ -57,16 +57,25 @@ class GroupTransformerEntropy(FairseqCriterion):
 
         lprobs = lprobs.view(-1, lprobs.size(-1))
         target = model.get_targets(sample, net_output).view(-1, 1)
+        # add eos loss
+        attns = net_output[1]['attn']
+        loss_eos = loss1 = attns[:,:,-1][:,:-1].sum()
 
         non_pad_mask = target.ne(self.padding_idx)
         nll_loss = -lprobs.gather(dim=-1, index=target)[non_pad_mask]
         smooth_loss = -lprobs.sum(dim=-1, keepdim=True)[non_pad_mask]
+        # remove reduce
+        '''
         if reduce:
             nll_loss = nll_loss.sum()
             smooth_loss = smooth_loss.sum()
+        '''
+        nll_loss = nll_loss.sum() + loss_eos
+        smooth_loss = smooth_loss.sum()
 
         eps_i = self.eps / lprobs.size(-1)
-        loss = (1. - self.eps) * nll_loss + eps_i * smooth_loss
+        loss = (1. - self.eps) * nll_loss + eps_i * smooth_loss 
+
 
         #-----------------------------------------------------------------------
         lprobs2 = lprobs2.view(-1,lprobs2.size(-1))
